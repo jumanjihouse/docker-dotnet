fa9f24e87675d0a3f802bb6b88ff0fab75a1687f
Paul Morgan <jumanjiman@gmail.com>
2015-11-24 09:43:23 -0500

    allow to pin installed versions

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -x
+set -eo pipefail
+
+source /VERSIONS
 
 # Install .Net Core Execution environment (DNX).
 mozroots --import --sync
 touch /home/user/.bash_profile
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev bash
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/${INSTALLER_VER}/dnvminstall.sh | bash
 source ~/.dnx/dnvm/dnvm.sh
 dnvm upgrade -u
-dnvm install -r coreclr latest -u
+dnvm install -r coreclr ${CORECLR_VER}
